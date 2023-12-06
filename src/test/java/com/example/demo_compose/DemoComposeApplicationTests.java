7c838ef63e83177b259b8e054c7e831110320f94
up1 <somkiat.p@gmail.com>
2023-12-06 20:36:59 +0700

    Demo project

---
+++ src/test/java/com/example/demo_compose/DemoComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_compose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoComposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
